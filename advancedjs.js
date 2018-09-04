46d1262ba80eab4440e1bace4e617755802eebf6
Morian <morian.uy@gmail.com>
2018-09-04 14:28:58 +0800

    added two more js files

---
+++ advancedjs.js
@@ -0,0 +1,197 @@
+//Create a one line function that adds adds two parameters
+const sum = (a, b) => a + b
+
+//Closure: What does the last line return?
+const addTo = x => y => x + y
+var addToTen = addTo(10)
+addToTen(3) // returns 13
+
+//Currying: What does the last line return?
+const sum = (a, b) => a + b
+const curriedSum = (a) => (b) => a + b
+curriedSum(30)(1) // 31
+
+
+//Currying: What does the last line return?
+const sum = (a, b) => a + b
+const curriedSum = (a) => (b) => a + b
+const add5 = curriedSum(5)
+add5(12) // 17
+
+//Composing: What does the last line return?
+const compose = (f, g) => (a) => f(g(a));
+const add1 = (num) => num + 1;
+const add5 = (num) => num + 5;
+compose(add1, add5)(10) // 16
+
+//What are the two elements of a pure function?
+1. Deterministic --> always produces the same results given the same inputs
+2. No Side Effects -->  It does not depend on any state, or data, change during a program’s execution. 
+It must only depend on its input elements.
+
+
+
+//ADVANCED ARRAY
+
+// Complete the below questions using this array:
+const array = [
+	{
+		username: "john",
+		team: "red",
+		score: 5,
+		items: ["ball", "book", "pen"]
+	},
+	{
+		username: "becky",
+		team: "blue",
+		score: 10,
+		items: ["tape", "backpack", "pen"]
+	},
+	{
+		username: "susy",
+		team: "red",
+		score: 55,
+		items: ["ball", "eraser", "pen"]
+	},
+	{
+		username: "tyson",
+		team: "green",
+		score: 1,
+		items: ["book", "pen"]
+	},
+
+];
+
+//Create an array using forEach that has all the usernames with a "!" to each of the usernames
+let newArray = []
+array.forEach(user => {
+	let { username } = user;
+	username = username + "!";
+	newArray.push(username);
+})
+console.log(newArray);
+
+//Create an array using map that has all the usernames with a "?" at the end of usernames
+const mapArray = array.map(user => {
+	let { username } = user;
+	return username + "?";
+})
+console.log(mapArray);
+
+//Filter the array to only include users who are on team: red
+const filterArray = array.filter(user => {
+	return user.team === "red";
+})
+console.log(filterArray);
+
+//Find out the total score of all users using reduce
+const total = array.reduce((acc, user) => {
+	return acc + user.score
+}, 0);
+console.log(total);
+
+
+// (1), what is the value of i? INDEX of the array.
+// (2), Make this map function pure:
+const arrayNum = [1, 2, 4, 5, 8, 9];
+const newArray = arrayNum.map((num, i) => {
+	return num * 2;
+})
+
+
+
+//BONUS: create a new list with all user information, but add "!" to the end of each items they own.
+const answer = array.map(user => {
+	user.items = user.items.map(item => {
+		return item + "!"
+	});
+	return user;
+})
+console.log(answer);
+
+//ADVANCED OBJECTS
+
+//Evaluate these:
+//#1
+[2] === [2] // false
+{} === {} // false
+
+//#2 what is the value of property a for each object.
+const object1 = { a: 5 }; // 4
+const object2 = object1; // 4
+const object3 = object2; // 4
+const object4 = { a: 5}; // 5
+object1.a = 4;
+
+
+//#3 create two classes: an Animal class and a Mamal class. 
+// create a cow that accepts a name, type and color and has a sound method that moo's her name, type and color. 
+class Animal {
+	constructor(name, type, color) {
+		this.name = name;
+		this.color = color;
+		this.type = type;
+	}
+}
+
+class Mamal extends Animal {
+	constructor(name, type, color) {
+		super(name, type, color)
+	}
+	sound() {
+		console.log(`Moooo I'm ${this.name} and I'm a ${this.color} ${this.type}`);
+	}
+}
+
+const cow = new Mamal('Shelly', 'cow', 'brown');
+
+
+//ES7 NEW FEATURES
+
+// #1) Check if this array includes the name "John".
+const dragons = ['Tim', 'Johnathan', 'Sandy', 'Sarah'];
+
+dragons.includes('John') // false
+
+// #2) Check if this array includes any name that has "John" inside of it. If it does, return that
+// name or names in an array.
+const dragons = ['Tim', 'Johnathan', 'Sandy', 'Sarah'];
+
+dragons.filter(name => name.includes('John')) // ['Johnathan']
+
+// #3) Create a function that calulates the power of 100 of a number entered as a parameter
+const power100 = (num) => num**100;
+
+// #4) Useing your function from #3, put in the paramter 10000. What is the result?
+// Research for yourself why you get this result.
+
+power100(10000) // Infinity
+
+
+//ES8 NEW FEATURES
+
+
+
+turtle = turtle.padStart(8);
+rabbit = rabbit.padStart(8);
+
+
+// #2) What happens when you run turtle.trim().padEnd(8, '=') on the turtle variable
+// Read about what the second parameter does in padEnd and padStart
+turtle = turtle.trim().padEnd(9, '=');
+
+'     ||<- Start line'
+'🐢======='
+'       🐇'
+
+
+// #3) Get the below object to go from:
+let obj = {
+  my: 'name',
+  is: 'Rudolf',
+  the: 'raindeer'
+}
+// to this:
+'my name is Rudolf the raindeer'
+
+Object.entries(obj).map(value => value.join(" ")).join(' ')
